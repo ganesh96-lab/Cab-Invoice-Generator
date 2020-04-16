3e9671011e4ed7eed09556b216b0a665d8369270
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-04-16 06:29:19 -0400

    REFACTOR:Optimize the code

--- src/test/java/InvoiceGeneratorTest.java
+++ src/test/java/InvoiceGeneratorTest.java
@@ -1,13 +1,19 @@
 import cabinvoicegenerator.InvoiceGenerator;
 import cabinvoicegenerator.Ride;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceGeneratorTest {
+    InvoiceGenerator invoiceGenerator=null;
+
+    @Before
+    public void setUp() throws Exception{
+        invoiceGenerator=new InvoiceGenerator();
+    }
 
     @Test
     public void whenGivenDistanceAndTime_shouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator=new InvoiceGenerator();
         double distance=2.0;
         int time=5;
         double fare=invoiceGenerator.calculateFare(distance,time);
@@ -17,7 +23,6 @@ public class InvoiceGeneratorTest {
 
     @Test
     public void givenMultipleRides_shouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator=new InvoiceGenerator();
         Ride[] rides={
                 new Ride(2.0,5),
                 new Ride(0.1,1)
